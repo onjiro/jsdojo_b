2e1cb5cdea04b7cf2d999f91aad9b8bffe87f371
Yoshiaki Sato <yoshiaki.st@gmail.com>
2013-02-23 17:35:24 +0900

    startでリストを追加する

--- public/js/view.app.js
+++ public/js/view.app.js
@@ -4,19 +4,26 @@ AppView = (function() {
             "click #start-pomodoro" : "startPomodoro"
         },
         initialize: function() {
+            
             this.currentPomodoro = new Pomodoro({
                 remainSeconds: this.model.pomounit,
             });
+            
             new CountdownView({
                 model: this.currentPomodoro,
                 el: $('#current_time'),
             });
+            
             this.currentPomodoro.on('change', function(){
                 if(this.currentPomodoro.get('status') === 'notstarted'){
                     $('#start-pomodoro').html('スタート！');
                 } else if (this.currentPomodoro.get('status') === 'started'){
                     $('#start-pomodoro').html('中断！');
                 }
+            }, this);
+            
+            this.currentPomodoro.on('start', function(){
+                var newListHtml = '<li>done1</li>';
             }, this);
         },
         
