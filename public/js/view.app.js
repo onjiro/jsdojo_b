b1982fb2d269f5dfc19d214c3740eb6954ba418f
Yoshiaki Sato <yoshiaki.st@gmail.com>
2013-02-23 16:12:19 +0900

    countdownView

--- public/js/view.app.js
+++ public/js/view.app.js
@@ -1,16 +1,41 @@
 AppView = (function() {
     return Backbone.View.extend({
         
+        
         events: {
             "click #start-pomodoro" : "startPomodoro"
         },
         
         startPomodoro : function(){
-            alert('test!');
+            alert('test0');
+            new Pomodoro({remainSeconds:25 * 60 * 1000});
+            alert('test01');
+            new CountdownView({el:$('#time')});
+            alert('test02');
         }
         
     });
 })();
+
+
+CountdownView = (function(){
+    return Backbone.View.extend({
+        
+        initialize:function(){
+            this.renewTimerHtml();
+        },
+        
+        timerHtml : '<span id="current_time">00:11</span>',
+        renewTimerHtml : function(){
+            this.$el.empty();
+            this.$el.append(this.timerHtml);
+        }
+        
+        
+    });
+})();
+
+
 
 $(function(){
     
